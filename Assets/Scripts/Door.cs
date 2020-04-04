38262c235639a56a58e08e0c6bbdb68f6405b9ff
Robert Parker <diaboloshogunate@gmail.com>
2020-04-04 03:51:59 -0700

    getting main scene set up with basic movement and input

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Sprite closed;
+    [SerializeField] private Sprite open;
+    private SpriteRenderer sprite;
+    private BoxCollider2D collider;
+    private bool isOpen = false;
+    
+    void Start()
+    {
+        this.sprite = GetComponent<SpriteRenderer>();
+        this.collider = GetComponent<BoxCollider2D>();
+    }
+
+    [Button]
+    public void Toggle()
+    {
+        this.isOpen = !this.isOpen;
+        this.sprite.sprite = this.isOpen ? this.open : this.closed;
+        this.collider.isTrigger = this.isOpen;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private GameObject colliderCheckPreview;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private float speed = 5f;
+    private Vector2 lastMovement;
+    private Vector2 movement = Vector2.right;
+
+    private void Start()
+    {
+        this.rb = GetComponent<Rigidbody2D>();
+        this.sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void Walk(InputAction.CallbackContext context)
+    {
+        this.movement = context.ReadValue<Vector2>();
+        if (this.movement.sqrMagnitude > 0f)
+        {
+            this.movement.Normalize();
+            this.rb.velocity = this.movement * this.speed;
+            this.colliderCheckPreview.transform.position = this.transform.position + (Vector3) this.movement;
+            if (this.movement.x != 0)
+            {
+                this.lastMovement = this.movement;
+                this.sprite.flipX = this.lastMovement.x < 0;
+            }
+        }
+        else
+        {
+            this.rb.velocity = Vector2.zero;
+        }
+    }
+    
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            Vector3 origin = this.transform.position;
+            if (this.movement.sqrMagnitude > 0)
+            {
+                origin = (Vector3) this.movement.normalized;
+            }
+            else
+            {
+                origin += (Vector3) this.lastMovement.normalized;
+            }
+            
+            Collider2D[] collisions = Physics2D.OverlapCircleAll(origin, 0.1f);//@TODO layermask?
+            foreach (Collider2D collision in collisions)
+            {
+                if (collision.gameObject.CompareTag("Door"))
+                {
+                    collision.gameObject.GetComponent<Door>().Toggle();
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
