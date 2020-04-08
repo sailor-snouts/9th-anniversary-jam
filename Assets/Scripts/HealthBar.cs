53fdbd9294ba4d5ad4e7e74a3d5f09588b825150
Robert Parker <diaboloshogunate@gmail.com>
2020-04-08 00:20:52 -0700

    title

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Slider slider = null;
+    private Image fill;
+    [SerializeField] private Gradient gradient = null;
+
+    private void Start()
+    {
+        this.slider = GetComponent<Slider>();
+        this.fill = this.slider.fillRect.GetComponent<Image>();
+        this.fill.color = this.gradient.Evaluate(1f);
+    }
+
+    public void SetMaxHealth(float value)
+    {
+        this.slider.maxValue = value;
+        this.fill.color = this.gradient.Evaluate(this.slider.normalizedValue);
+    }
+
+    public void SetHealth(float value)
+    {
+        this.slider.value = value;
+        this.fill.color = this.gradient.Evaluate(this.slider.normalizedValue);
+    }
+}
---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+          Application.Quit();
+#endif
+    }
+}
