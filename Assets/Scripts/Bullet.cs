a1a98ec4a645fb95ec805d5029f7b3066bf35062
Robert Parker <diaboloshogunate@gmail.com>
2020-04-14 20:48:33 -0700

    zombie death and bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,23 @@ public class Bullet : MonoBehaviour
 {
     private Rigidbody2D rb = null;
     private float speed = 5f;
-    private Vector2 direction = Vector2.zero;
+    [SerializeField] private LayerMask collisionLayers = 0;
+    [SerializeField] private float dmg = 1;
 
     public void Fire(Vector2 direction)
     {
         direction.Normalize();
-        Quaternion.Euler(0f, 0f, -Mathf.Atan2(this.direction.x, this.direction.y) * Mathf.Rad2Deg);
+        this.transform.rotation = Quaternion.Euler(0f, 0f, -Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg - 90f);
         this.rb = GetComponent<Rigidbody2D>();
         this.rb.velocity = direction * this.speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (this.collisionLayers != (this.collisionLayers | (1 << other.gameObject.layer))) return;
+        Health health = other.gameObject.GetComponent<Health>();
+        if(health) { health.Damage(this.dmg); }
+        Debug.Log(other.gameObject);
+        Destroy(this.gameObject);
     }
 }
